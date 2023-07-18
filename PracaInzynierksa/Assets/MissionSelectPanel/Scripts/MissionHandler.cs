8a1cd3e54c69efc6850d6e16b75fe3607908e647
MarcinLarek <94295943+MarcinLarek@users.noreply.github.com>
2023-07-18 17:03:15 +0200

    Generowanie sie misji i przechodzenie do mapy

--- PracaInzynierksa/Assets/MissionSelectPanel/Scripts/MissionHandler.cs
+++ PracaInzynierksa/Assets/MissionSelectPanel/Scripts/MissionHandler.cs
@@ -1,8 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Device;
 
 public class MissionHandler : MonoBehaviour
 {
-    
+    private static MissionHandler instance;
+    public static MissionHandler GetInstance()
+    {
+        return instance;
+    }
+
+    public List<GameObject> missionsList;
+    public GameObject spawningSpace;
+    public int numberToSpawn;
+    private int numberOfSpawned;
+
+    private void Awake()
+    {
+        instance = this; // Singleton
+        spawnMissions();
+    }
+
+    private void spawnMissions()
+    {
+        //Poniewaz uzyto troche mentalnej gimnastyki, mozliwe jest zespawnowanie sie minimalnego wyniku - 1 misji.
+        //Na przyklad jesli mamy minimum 2 misje i druga misja umiesci sie w tym samym miejscu co pierwsza
+        //to druga misja zostanie usunieta.
+        numberToSpawn = Random.Range(2, 5);
+        Debug.Log("Spawned missions: " + numberToSpawn);
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            spawnSingleMission();
+        }
+    }
+
+    private void spawnSingleMission()
+    {
+        //Bierzemy liste mozliwych misji/typow misji do wylosowania
+        GameObject missionToSpawn = missionsList[Random.Range(0, missionsList.Count)];
+        //Obszar na jakim moze byc wygenerowany marker
+        MeshCollider renderSpace = spawningSpace.GetComponent<MeshCollider>();
+        float spawnPositionX, spawnPositionY;
+        Vector2 spawnposition;
+
+        spawnPositionX = Random.Range(renderSpace.bounds.min.x, renderSpace.bounds.max.x);
+        spawnPositionY = Random.Range(renderSpace.bounds.min.y, renderSpace.bounds.max.y);
+        spawnposition = new Vector2(spawnPositionX, spawnPositionY);
+
+        GameObject spawnedMarker = Instantiate(missionToSpawn, spawnposition, missionToSpawn.transform.rotation);
+        numberOfSpawned += 1;
+        //Przypisujemy kolejny numer po to aby przy wyrkyciu kolizji uzyc go do usuniecia tylko jednego markera
+        //Obencie usuwamy ten z wiekszym numerem
+        spawnedMarker.GetComponent<MissionMarkerBehaviour>().spawnedNumber = numberOfSpawned;
+    }
+
 }
---
+++ PracaInzynierksa/Assets/MissionSelectPanel/Scripts/MissionMarkerBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MissionMarkerBehaviour : MonoBehaviour
+{
+    private MissionHandler missionHandler = MissionHandler.GetInstance();
+
+    public int spawnedNumber;
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName: "MapGenerator");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Glupie i proste rozwiazanie po to aby usunac tylko 1 z kolidujacych obiektow
+        if(spawnedNumber > collision.gameObject.GetComponent<MissionMarkerBehaviour>().spawnedNumber)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
