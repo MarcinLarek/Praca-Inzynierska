c48cae919d7ae0703687ee3e3e61341cce469570
MarcinLarek <94295943+MarcinLarek@users.noreply.github.com>
2023-08-07 20:14:56 +0200

    Rozwijanei umiejetnosci postaci za expa
    ToDo: Zapisywanei postaci po lvlowaniu

--- PracaInzynierksa/Assets/CrewUpgrade/Scripts/UpgradeButtonBehaviour.cs
+++ PracaInzynierksa/Assets/CrewUpgrade/Scripts/UpgradeButtonBehaviour.cs
@@ -1,16 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class UpgradeButtonBehaviour : MonoBehaviour
+public class UpgradeButtonBehaviour : MonoBehaviour, IPointerClickHandler
 {
-    private void OnMouseDown()
+    public void OnPointerClick(PointerEventData eventData)
+
     {
-        StatSelector();
+        StatUpgrade();
+        Debug.Log("Click!");
     }
 
-    private void StatSelector()
+    private void StatUpgrade()
     {
+        CharacterStats activeCharacterStats = UpgradeManager.GetInstance().activeCharacter.GetComponent<CharacterStats>();
+        Debug.Log(this.name);
+        switch (this.name) 
+        {
+            case "Upgrade-Health":
+                activeCharacterStats.experience -= activeCharacterStats.maxHealth * 100;
+                activeCharacterStats.maxHealth++;
+                activeCharacterStats.health = activeCharacterStats.maxHealth;
+                break;
+            case "Upgrade-ActionPoints":
+                activeCharacterStats.experience -= activeCharacterStats.maxActionPoints * 100;
+                activeCharacterStats.maxActionPoints++;
+                activeCharacterStats.actionPoints = activeCharacterStats.maxActionPoints;
+                break;
+            case "Upgrade-Strength":
+                activeCharacterStats.experience -= activeCharacterStats.strength * 100;
+                activeCharacterStats.strength++;
+                break;
+            case "Upgrade-Endurance":
+                activeCharacterStats.experience -= activeCharacterStats.endurance * 100;
+                activeCharacterStats.endurance++;
+                break;
+            case "Upgrade-Agility":
+                activeCharacterStats.experience -= activeCharacterStats.agility * 100;
+                activeCharacterStats.agility++;
+                break;
+            case "Upgrade-Luck":
+                activeCharacterStats.experience -= activeCharacterStats.luck * 100;
+                activeCharacterStats.luck++;
+                break;
+            case "Upgrade-Inteligence":
+                activeCharacterStats.experience -= activeCharacterStats.inteligence * 100;
+                activeCharacterStats.inteligence++;
+                break;
+            default:
+                Debug.Log("Something Wrong with upgrade buttons");
+                break;
 
+        }
     }
 }
