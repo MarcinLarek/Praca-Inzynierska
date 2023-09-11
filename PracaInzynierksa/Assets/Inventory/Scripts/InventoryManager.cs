1fbf10204e4869ead7f3e7f04437603d55d7aff6
MarcinLarek <94295943+MarcinLarek@users.noreply.github.com>
2023-09-11 18:43:43 +0200

    Bugfixy Ekwipunku, scena handlu

--- PracaInzynierksa/Assets/Inventory/Scripts/InventoryManager.cs
+++ PracaInzynierksa/Assets/Inventory/Scripts/InventoryManager.cs
@@ -12,7 +12,7 @@ public class InventoryManager : MonoBehaviour
 
     private void Awake()
     {
-        //LoadFromGameHandler();
+        LoadFromGameHandler();
     }
 
     private void LoadFromGameHandler()
