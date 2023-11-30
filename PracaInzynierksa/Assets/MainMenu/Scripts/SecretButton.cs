0ac4fc3f58ea58c2ee1a8da2907fa7f381979d38
MarcinLarek <94295943+MarcinLarek@users.noreply.github.com>
2023-11-30 23:27:40 +0100

    WaznyPrzycisk

---
+++ PracaInzynierksa/Assets/MainMenu/Scripts/SecretButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretButton : MonoBehaviour
+{
+    public void SecretButtonFunction()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
